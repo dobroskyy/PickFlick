a436334789f33299efee5e9f94100c4f004d3089
dobroskyy <dobrozhinsky@gmail.com>
2025-09-17 16:36:06 +0300

    Initial Commit

---
+++ PickFlick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PickFlick
+//
+//  Created by Максим Доброжинский on 17.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
